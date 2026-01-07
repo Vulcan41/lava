c2a2f22b519c884a531c7c44ffdd6a123f3ecf53
Vulcan41 <161815679+Vulcan41@users.noreply.github.com>
2026-01-07 15:28:56 +0200

    Delete docs directory

--- docs/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://esm.sh/supabase/supabase-js2";
-
-const SUPABASE_URL = "https://jqzqbepwafgmkuajjkll.supabase.cohttps://jqzqbepwafgmkuajjkll.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxenFiZXB3YWZnbWt1YWpqa2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MDU4ODAsImV4cCI6MjA4MzI4MTg4MH0.sHAUHBwE0lIZuhU9CAdQmN952tYHJNOhlkumFiAmUes";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
